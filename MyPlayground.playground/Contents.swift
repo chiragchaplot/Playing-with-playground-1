16bd69e195b7e10add14c726c73dc8c4c6604bd6
Chirag Chaplot <chiragchaplot@gmail.com>
2016-10-22 20:32:54 +0300

    More of playgrounds
    Getting used to variables and data types

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -3,3 +3,26 @@
 import UIKit
 
 var str = "Hello, playground"
+var name:String = "Chirag Chaplot"
+
+print("hello " +  name + "!")
+
+var int:Int = 9
+
+int = int * 2
+
+int = int / 2
+
+int = int / 4
+
+var anotherint = 7
+
+print(int + anotherint)
+
+var number:Double = 8.4
+
+print(number * Double(int))
+
+print ("Answer to multiplication is \(number * Double(int))")
+
+var isMale:Bool = true
