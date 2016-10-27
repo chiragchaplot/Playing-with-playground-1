0ec147db98ecef6d02e7757693a079f2776ea111
Chirag Chaplot <chiragchaplot@gmail.com>
2016-10-27 10:08:28 +0300

    Arrays and Dictionaries
    Arrays and dictionaries done

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -26,3 +26,45 @@ print(number * Double(int))
 print ("Answer to multiplication is \(number * Double(int))")
 
 var isMale:Bool = true
+
+//Arrays
+
+var array = [17, 25, 13, 47]
+
+print(array[0])
+print (array.count)
+array.removeAtIndex(0)
+
+print (array)
+
+array.sort()
+
+//----------------------------------------------------------
+
+var dictionary = ["computer":"something to play of Call of Duty on", "coffee":"best drink ever"]
+
+print (dictionary["coffee"])
+
+//To remove optional add the !
+print(dictionary["coffee"]!)
+
+print(dictionary.count)
+
+
+dictionary["pen"] = "Old fashioned writing implement"
+
+dictionary.removeValueForKey("computer")
+
+print(dictionary)
+
+var prices = ["pepsi":1.5, "chips":1.0, "pizza":10.99]
+
+//Don't forget to add ! as it gives an error
+var totalcost = prices["pepsi"]! + prices["chips"]! + prices["pizza"]!
+
+print("total cost of 3 items is \(totalcost)")
+
+//----------------------------------------------------------
+
+
+
